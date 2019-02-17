2a05b1dc8dfba9eaf8408e3cf1fcf8f77c9e0400
Michael <michael@managedkaos.com>
2019-02-17 11:31:02 -0800

    Change INSTALL_LOG to use ebwiki.log; Add fakes3 configs

--- dev_provisions/environment.sh
+++ dev_provisions/environment.sh
@@ -1,6 +1,6 @@
 export EBWIKI_SITEMAP_URL=http://bow-sitemaps.s3.amazonaws.com/sitemaps/sitemap.xml.gz
 export PROJECT_URL='http://192.168.68.68'
-export INSTALL_LOG='/tmp/install.log'
+export PROJECT_LOG='/vagrant/ebwiki.log'
 export DEBIAN_FRONTEND=noninteractive
 export BLACKOPS_DATABASE_PASSWORD='ebwiki'
 export DATABASE_DUMP_FILE='latest.dump'
@@ -12,6 +12,9 @@ export SEARCHBOX_URL=''
 export CODECLIMATE_REPO_TOKEN=''
 export AUTOBUS_SNAPSHOT_URL=''
 
-# AWS Credentials for FakeS3.  Please don't change!
+# Configuration for FakeS3.  Please don't change!
+export FAKE_S3_HOME=/tmp/fakes3
+export FAKE_S3_PORT=4567
+export FAKE_S3_KEY=1908855126
 export AWS_ACCESS_KEY_ID='accessKey1'
 export AWS_SECRET_KEY_ID='verySecretKey1'

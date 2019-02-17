9f2ec426fe97117f8fbf51dda1fec2d9c4827bc7
Michael <michael@managedkaos.com>
2019-02-17 11:31:43 -0800

    Add db:migrate step

--- dev_provisions/provision_database.sh
+++ dev_provisions/provision_database.sh
@@ -1,6 +1,7 @@
 source /vagrant/dev_provisions/environment.sh
 echo '## Provisioning database ...'
+cd /vagrant
 echo "postgres://blackops:${BLACKOPS_DATABASE_PASSWORD}@localhost:5432/blackops_development /vagrant/${DATABASE_DUMP_FILE}"
 sleep 2
 pg_restore --verbose --clean --no-acl --no-owner --no-password --dbname="postgres://blackops:${BLACKOPS_DATABASE_PASSWORD}@localhost:5432/blackops_development" "/vagrant/${DATABASE_DUMP_FILE}"
-
+bin/rake db:migrate RAILS_ENV=development
